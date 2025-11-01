1acbda1ccf9a0871d87856cc7e7b6084c94b528b
Alexander Kozhukhar <ak@uloc.ru>
2025-11-01 12:44:45 +0300

    configure/manifest revision, marked at me_slot_config.c

--- components/me_slot_config/me_slot_config.c
+++ components/me_slot_config/me_slot_config.c
@@ -85,113 +85,113 @@ int init_slots(void){
 		if(!memcmp(me_config.slot_mode[i], "audioPlayer", 11)){
 			audioInit(i);
 		}else if(!memcmp(me_config.slot_mode[i], "audioLAN", 8)){
-			start_audioLAN_task(i); 		// OK
+			start_audioLAN_task(i); 		// W, C
 		}else if(!memcmp(me_config.slot_mode[i], "button_ledRing", 14)){
-			start_button_task(i); 		// OK
-			start_ledRing_task(i); 		// OK
+			start_button_task(i); 		// W, C
+			start_ledRing_task(i); 		// W, C
 		}else if(!memcmp(me_config.slot_mode[i], "button_ledBar", 13)){
-			start_button_task(i); 		// OK
-			start_ledBar_task(i);  		// OK
+			start_button_task(i); 		// W, C
+			start_ledBar_task(i);  		// W, C
 		}else if(!memcmp(me_config.slot_mode[i], "button_swiperLed", 16)){
-			start_button_task(i);		// OK
-			start_swiperLed_task(i);	// OK
+			start_button_task(i);		// W, C
+			start_swiperLed_task(i);	// W, C
 		}else if(!memcmp(me_config.slot_mode[i], "button_smartLed", 15)){
-			start_button_task(i); 		// OK
-			start_smartLed_task(i);		// OK
+			start_button_task(i); 		// W, C
+			start_smartLed_task(i);		// W, C
 		}else if(!memcmp(me_config.slot_mode[i], "button_led", 10)){
-			start_button_task(i);		// OK
-			start_led_task(i);			// OK
+			start_button_task(i);		// W, C
+			start_led_task(i);			// W, C
 		}else if(!memcmp(me_config.slot_mode[i], "in_3ch", 6)){
-			start_in_3ch_task(i, 3);	// OK
+			start_in_3ch_task(i, 3);	// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "in_2ch", 6)){
-			start_in_3ch_task(i, 2);	// OK
+			start_in_3ch_task(i, 2);	// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "in_1ch", 6)){
-			start_in_3ch_task(i, 1);	// OK
+			start_in_3ch_task(i, 1);	// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "pwmRGB", 6)){
-			init_3n_mosfet(i);			// OK
+			init_3n_mosfet(i);			// W, C
 		// }else if(!memcmp(me_config.slot_mode[i], "encoderPPM", 10)){
-		// 	start_encoderPPM_task(i);	// OK
+		// 	start_encoderPPM_task(i);	// W
 		}else if(!memcmp(me_config.slot_mode[i], "encoderInc", 10)){
-			start_encoder_inc_task(i);	// OK
+			start_encoder_inc_task(i);	// W, C
 		}else if(!memcmp(me_config.slot_mode[i], "encoderAS5600", 13)){
-			start_encoderAS5600_task(i);
+			start_encoderAS5600_task(i); // W, C
 		}else if(!memcmp(me_config.slot_mode[i], "benewakeTOF", 12)){
-			start_benewakeTOF_task(i);	// OK
+			start_benewakeTOF_task(i);	// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "VL53TOF", 7)){
-			start_VL53TOF_task(i);		// OK
+			start_VL53TOF_task(i);		// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "hlk2410", 7)){
-			start_hlk2410_task(i);		// OK
+			start_hlk2410_task(i);		// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "sr04m", 5)){
-			start_ultrasonic_task(i);	// OK
+			start_ultrasonic_task(i);	// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "tachometer", 10)){
-			start_tachometer_task(i);	// OK
+			start_tachometer_task(i);	// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "analog", 6)){
-			start_analog_task(i);
+			start_analog_task(i); // W, C
 		}else if(!memcmp(me_config.slot_mode[i], "stepper", 7)){
-			start_stepper_task(i);		// OK
+			start_stepper_task(i);		// W, C
 		}else if(!memcmp(me_config.slot_mode[i], "in_out", 6)){
-			start_out_task(i);			// OK
-			start_in_task(i);			// OK
+			start_out_task(i);			// W, NOC
+			start_in_task(i);			// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "out_3ch", 7)){
-			start_out_3ch_task(i);		// OK
+			start_out_3ch_task(i);		// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "out_2ch", 7)){
-			start_out_2ch_task(i);		// OK
+			start_out_2ch_task(i);		// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "tenzoButton", 12)){
-			start_tenzo_button_task(i);	// OK
+			start_tenzo_button_task(i);	// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "flywheel", 7)){
-			start_flywheel_task(i);		//OK
+			start_flywheel_task(i);		//OK, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "scaler", 6)){
-			start_scaler_task(i);		// OK
+			start_scaler_task(i);		// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "swiper", 6)){
-			start_swiper_task(i);		// OK
+			start_swiper_task(i);		// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "rfid", 4)){
-			start_pn532Uart_task(i);	// OK
+			start_pn532Uart_task(i);	// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "startup", 8)){
-			start_startup_task(i);		// ???
+			start_startup_task(i);		// NW, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "HID", 3)){
-			start_HID_task(i);			// OK
+			start_HID_task(i);			// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "counter", 3)){
-			start_counter_task(i);		// OK
+			start_counter_task(i);		// W, C
 		}else if(!memcmp(me_config.slot_mode[i], "timer", 3)){
-			start_timer_task(i);		// OK
+			start_timer_task(i);		// W, C
 		}else if(!memcmp(me_config.slot_mode[i], "watchdog", 3)){
-			start_watchdog_task(i);		// OK
+			start_watchdog_task(i);		// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "uartLogger", 3)){
-			start_uartLogger_task(i);   // OK
+			start_uartLogger_task(i);   // W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "buttonMatrix", 13)){
-			start_buttonMatrix_task(i);	// OK
+			start_buttonMatrix_task(i);	// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "dwin", 4)){
-			start_dwinUart_task(i);		// OK
+			start_dwinUart_task(i);		// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "dialer", 7)){
-			start_dialer_task(i);		// OK
+			start_dialer_task(i);		// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "whitelist", 9)){
-			start_whitelist_task(i);	// OK
+			start_whitelist_task(i);	// W, C
 		}else if(!memcmp(me_config.slot_mode[i], "collector", 9)){
-			start_collector_task(i);	// OK
+			start_collector_task(i);	// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "random", 6)){
-			start_random_task(i);	// OK
+			start_random_task(i);	// W, C
 		}else if(!memcmp(me_config.slot_mode[i], "ds18b20", 7)){
-			start_ds18b20_task(i);		// OK
+			start_ds18b20_task(i);		// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "servoRod", 9)){
-			start_servoRod_task(i);		// OK
+			start_servoRod_task(i);		// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "steadywinGIM", 12)){
-			start_GIM_motor_task(i);	// OK
+			start_GIM_motor_task(i);	// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "ticketDispenser", 15)){
-			start_ticketDispenser_task(i);	// OK
+			start_ticketDispenser_task(i);	// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "volnaKolya", 10)){
-			start_volnaKolya_task(i);	// OK
+			start_volnaKolya_task(i);	// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "CRSF", 4)){
-			start_crsf_rx_task(i);		// OK
+			start_crsf_rx_task(i);		// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "VESC", 4)){
 			start_CAN_VESC_task(i);		// ???
 		}else if(!memcmp(me_config.slot_mode[i], "PPM", 3)){
-			start_ppm_generator_task(i);// OK
+			start_ppm_generator_task(i);// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "tankControl", 11)){
-			start_tankControl_task(i);	// OK
+			start_tankControl_task(i);	// W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "furbyEye", 8)){
-			start_furbyEye_task(i);
+			start_furbyEye_task(i); // W, NOC
 		}else if(!memcmp(me_config.slot_mode[i], "conductor", 9)){
-			start_stepper_conductor_task(i);
+			start_stepper_conductor_task(i); // NOW, NOC
 		}
 		
 	}
